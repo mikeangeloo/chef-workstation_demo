f8b3c0e0b12e0607e0418aacfaa463efe8b1326f
Miguel Angel Ramirez Lopez <miguelangel@MacBook-Air-de-Miguel.local>
2024-01-19 22:22:35 -0500

    some unit test examples with chefspec

--- cookbooks/apache/recipes/facts.rb
+++ cookbooks/apache/recipes/facts.rb
@@ -1,4 +1,6 @@
 log 'Showing machine Ohai attributes' do
-    message "Machine with #{node['memory']['total']} of memory and #{node['cpu']['total']} processor/s. \
-    \nPlease check access to http://#{node[:network][:interfaces][:ens256][:addresses].detect{|k,v| v[:family] == 'inet'}.first}"
+    message "Machine with #{node['memory']['total']} of memory and #{node['cpu']['total']} processor/s. \    
+    \nPlease check access to http://#{node[:network][:interfaces].first}"
 end
+
+#\nPlease check access to http://#{node[:network][:interfaces][:ens256][:addresses].detect{|k,v| v[:family] == 'inet'}.first}"
--- cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -7,23 +7,26 @@
 require 'spec_helper'
 
 describe 'apache::default' do
+  platform 'ubuntu'
+
+  # let(:chef_run) { ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '20.04').converge(described_recipe) }
+
   context 'When all attributes are default, on Ubuntu 18.04' do
     # for a complete list of available platforms and versions see:
     # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'ubuntu', '18.04'
+    
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7' do
-    # for a complete list of available platforms and versions see:
-    # https://github.com/chefspec/fauxhai/blob/master/PLATFORMS.md
-    platform 'centos', '7'
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'includes the apache2 recipe' do
+      expect(chef_run).to include_recipe('apache::default')
     end
+
+    it 'should have vagrant variable' do
+      expect(chef_run.node['apache']['document_root']).to eq('/vagrant')
+    end
+
   end
 end
