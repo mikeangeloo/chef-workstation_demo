8008bbaeecb176bb38934447b7f49a307b803ae5
Miguel Angel Ramirez Lopez <mikeangeloo87@outlook.com>
2024-01-20 11:33:11 -0500

    adding document links

--- cookbooks/apache/test/integration/apache/apache_spec.rb
+++ cookbooks/apache/test/integration/apache/apache_spec.rb
@@ -1,6 +1,10 @@
 # Additional documentation: https://docs.chef.io/inspec/shell/
 # https://docs.chef.io/inspec/plugin_kitchen_inspec/
 # https://docs.chef.io/inspec/shell/
+# https://docs.chef.io/inspec/resources/
+
+# deprecated
+# https://serverspec.org/resource_types.html
 
 if os[:family] == 'ubuntu'
     describe package('apache2') do
