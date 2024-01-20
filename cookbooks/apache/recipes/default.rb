5e01e9ea623e41cde8d63ddf5e3301b2c779c7ba
Miguel Angel Ramirez Lopez <mikeangeloo87@outlook.com>
2024-01-20 13:33:09 -0500

    Results of activiy 2

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -37,4 +37,59 @@ end
 #     end
 # end
 
+cookbook_file '/etc/apache2/sites-available/ejemplo.conf' do
+    action :create
+end
+    
+
 include_recipe '::facts'
+
+#### act2
+cookbook_file '/etc/apache2/sites-available/wordpress.conf' do #does to do
+  source 'wordpress.conf'
+  action :create
+  notifies :restart, resources(:service => "apache2")
+end
+ 
+link '/etc/apache2/sites-enabled/wordpress.conf' do #cookbook_link to link
+   to '/etc/apache2/sites-available/wordpress.conf'
+   notifies :restart, resources(:service => "apache2")
+end
+ 
+directory '/srv/www/wordpress' do
+  action :create # new to create
+end
+ 
+# execute 'cp wordpress' do #exec to execute
+#   action :run
+#   command 'cp -r /wordpress/* /srv/www/wordpress'
+# end
+
+cookbook_file "#{node['apache']['document_root']}/script.py" do
+    source 'script.py'
+end
+
+execute 'run python script' do
+    action :run
+    command 'python3 /vagrant/script.py'
+end
+
+# python 'hello world' do
+#     code <<-EOH
+#       print "Hello world! From Chef and Python."
+#     EOH
+#   end
+
+cron 'runcron' do
+    minute '0'
+    hour '7'
+    weekday '5'
+    mailto 'done@done.es'
+    action :create
+    command 'echo skippin'
+  end
+  
+  
+  
+    
+
